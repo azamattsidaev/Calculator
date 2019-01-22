03cb4d98a94debb43428e0186a8e883de4e21c98
azamattsidaev <46766262+azamattsidaev@users.noreply.github.com>
2019-01-22 16:43:01 -0500

    REMOTE: add divide changes

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -11,5 +11,11 @@ public class Calculator1 {
     public static double multiply(double num1, double num2){
         return num1 * num2;
     }
+   
+    
+    public static double divide (double num1, double num2){
+        return num1 / num2;
+    }
+    
     
 }
