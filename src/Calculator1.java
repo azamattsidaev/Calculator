cbfc21abbc4e5c23c46787db3e6584d998bea7a3
Azamat Tcidaev <atcidaev@gmail.com>
2019-01-22 17:01:31 -0500

    add local comments

--- src/Calculator1.java
+++ src/Calculator1.java
@@ -12,9 +12,10 @@ public class Calculator1 {
     public static double multiply(double num1, double num2){
         return num1 * num2;
     }
-
+  //local repo: commebt 1
     public static double divide (double n1, double n2){
         double result =  n1 / n2;
         return  result;
     }
+// local repo: comment 2
 }
